56244c84f01f2760b6e12bb4c0f9e348f546ab6a
Luis Medel <luis@medel.email>
2022-05-12 06:45:24 +0200

    Print errors and warnings on stderr

--- Clarin/Log.cs
+++ Clarin/Log.cs
@@ -4,7 +4,7 @@ namespace Clarin
     {
         public static void Trace(string text) => System.Diagnostics.Trace.WriteLine("[TRACE]" + text);
         public static void Info (string text) => System.Console.WriteLine (text);
-        public static void Warn (string text) => System.Console.WriteLine ("[WARN] " + text);
-        public static void Error (string text) => System.Console.WriteLine ("[ERROR] " + text);
+        public static void Warn (string text) => System.Console.Error.WriteLine ("[WARN] " + text);
+        public static void Error (string text) => System.Console.Error.WriteLine ("[ERROR] " + text);
     }
 }
